71d120137031ef83bd03583aa8e03b2a08dd0ca3
kc03kc03 <113570132+kc03kc03@users.noreply.github.com>
2022-09-18 20:48:38 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 //If player 1 wins
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩 computer Wins!";
+  document.querySelector("h1").innerHTML = "🚩 Mo Nei Meow Wins!";
 }
 else if (randomNumber2 > randomNumber1) {
   document.querySelector("h1").innerHTML = "Player Wins! 🚩";
