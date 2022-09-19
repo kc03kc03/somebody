2c42e6215a4027fec79b988b5001edd7a6161a65
kc03kc03 <113570132+kc03kc03@users.noreply.github.com>
2022-09-18 22:34:22 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ image1.setAttribute("src", randomImageSource);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+var randomImageSource2 = "images/cat" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 
 //If player 1 wins
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩 Mo Nei Meow Wins!";
+  document.querySelector("h1").innerHTML = "🚩 Mo Nei Meow ^o^ Wins!";
 }
 else if (randomNumber2 > randomNumber1) {
   document.querySelector("h1").innerHTML = "Player Wins! 🚩";
